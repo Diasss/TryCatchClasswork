f265326228d88faad30228e629710060687407cd
Диас Оспанов <@local.itstep>
2019-02-18 18:05:37 +0600

    Добавьте файлы проекта.

---
+++ tryCatch/LogTableOverFlow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatch
+{
+    public class LogTableOverFlow : Exception
+    {
+        const string overflowMessage = "The log table has overflowed";
+        public LogTableOverFlow(string auxMessage, Exception inner) : base(string.Format("{0} - {1}", overflowMessage, auxMessage), inner)
+        {
+            this.HelpLink = "http://google.com";
+            this.Source = "Exception_Class_Samples";
+        }
+
+    }
+    public class LogTable
+    {
+        protected string[] logArea;
+        protected int elemntUse;
+        public LogTable(int numElements)
+        {
+            logArea = new string[numElements];
+            elemntUse = 0;
+        }
+        public int Addrecord(string newRecord)
+        {
+            try
+            {
+                logArea[elemntUse] = newRecord;
+                return elemntUse++;
+            }
+            catch (Exception e)
+            {
+                throw new LogTableOverFlow(string.Format("Record \"{0}\"was not logged", newRecord), e);
+            }
+            finally
+            {
+                //ОТРАБОТАЕТ ВСЕГДА!
+            }
+        }
+    }
+}
+ 
---
+++ tryCatch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatch
+{
+    class Program
+    {
+
+        static int[] num = { 2, 4, 5, 6, 7 };
+
+        public static int GetNum(int index)
+        {
+            if (index < 0 || index >= num.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return num[index];
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                GetNum(66);
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+
+            int x = 1;
+            int y = 0;
+            try
+            {
+                int result = x / y;
+            }
+            catch (Exception ex)
+            when(y==0)
+            {
+
+                Console.WriteLine("y не должен быть равен нулю");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            LogTable log = new LogTable(4);
+            Console.WriteLine("---");
+            try
+            {
+                for (int count = 1; ; count++)
+                {
+                    log.Addrecord(string.Format("log record number {0}", count));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nMessage----n{0}", ex.Message);
+                Console.WriteLine("\nHelp link---\n{0}", ex.HelpLink);
+                Console.WriteLine("\nSource ---\n{0}", ex.Source);
+                Console.WriteLine("\nStackTrace---\n{0}", ex.StackTrace);
+                Console.WriteLine("\nTargerSite---\n{0}", ex.TargetSite);
+            }
+        }
+    }
+}
